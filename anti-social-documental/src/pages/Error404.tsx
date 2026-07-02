35c0dd9f0b994a761cfce1d0a56b15e569971602
Nicolas389 <nicolasjgonzalez389@gmail.com>
2026-07-02 17:23:05 -0300

    Actualizar Error404 con imagen del gato desde URL externa

--- anti-social-documental/src/pages/Error404.tsx
+++ anti-social-documental/src/pages/Error404.tsx
@@ -1,12 +1,11 @@
 import { Container } from "react-bootstrap"
-import ErrorImage from "../assets/404.gray.png"
 import "../styles/error404.css"
 
 const Error404 = () => {
     return(
         <Container className="error404-container">
             <img
-                src={ErrorImage}
+                src="https://img.magnific.com/vector-premium/pagina-error-gato-gatito-dormido-caja-signo-404-paginas-vacias-no-encontradas-problemas-internet-computadora-perdio-sitio-web-dibujos-animados-gatito-gordito-espacio-blanco-ilustracion-vectorial-problema-internet-pagina_81894-11672.jpg?semt=ais_hybrid&w=740&q=80"
                 alt="404 Not Found"
                 className="error-image"
             />
