4b62947d0f181401050ff7d24eb470e027d4d496
joelalan13 <joel-alan13@hotmail.com>
2026-07-02 16:57:53 -0300

    pagina para error404, tiene su config comentada para pasarse a index.css

---
+++ anti-social-documental/src/pages/Error404.jsx
@@ -0,0 +1,20 @@
+import { Container } from "react-bootstrap"
+import ErrorImage from "../assets/404.gray.png"
+
+{/*.error-image {
+    width: min(80vw, 350px);
+    height: auto;
+}*/}
+const Error404 = () =>{
+    return(
+        <Container className="mt-5 text-center">
+            <img
+                src={ErrorImage}
+                alt="404 Not Found"
+                className="error-image"
+            />
+        </Container>
+    )   
+}
+
+export default Error404
